b51c26c42bcf26c87b9db05e6096ecf84da9f775
github username <kevinladkins@gmail.com>
2017-09-25 13:58:43 -0400

    Comitted

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
+<<<<<<< HEAD
 var path = require('path');
 var webpack = require('webpack');
 var autoprefixer = require('autoprefixer');
 var precss = require('precss');
+=======
+var path = require("path");
+var webpack = require("webpack");
+>>>>>>> 6b783ca151f566f87c537becd58f1b015d3a9b5f
 
 module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
+<<<<<<< HEAD
     'webpack/hot/only-dev-server',
+=======
+>>>>>>> 6b783ca151f566f87c537becd58f1b015d3a9b5f
     './src/index'
   ],
   output: {
@@ -19,6 +27,7 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+<<<<<<< HEAD
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.LoaderOptionsPlugin({
         test: /\.scss$/,
@@ -32,13 +41,22 @@ module.exports = {
   ],
   module: {
     rules: [
+=======
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+>>>>>>> 6b783ca151f566f87c537becd58f1b015d3a9b5f
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loaders: ['babel-loader']
+<<<<<<< HEAD
       }, {
         test: /\.scss$/,
         loaders: ['style-loader', 'css-loader', 'postcss-loader']
+=======
+>>>>>>> 6b783ca151f566f87c537becd58f1b015d3a9b5f
       }
     ]
   }
