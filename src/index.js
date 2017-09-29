4919f78a835f3983c49f8551e623de7da946ce21
github username <kevinladkins@gmail.com>
2017-09-29 19:34:59 -0400

    Build GalleryItem component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App/App'
+import styles from './components/App/App.scss'
 
 let element = React.createElement(App, {});
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 
 var path = require("path");
 var webpack = require("webpack");
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
 
 module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
     './src/index'
   ],
   output: {
@@ -15,17 +18,32 @@ module.exports = {
   resolve: {
     extensions: ['*', '.js', '.jsx']
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-  ],
   module: {
-    loaders: [
+    rules: [
+      {
+        test: /\.scss$/,
+        include: path.resolve('./src'),
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'},
+          {loader: 'postcss-loader',
+           options: {
+             plugins: function () { // post css plugins, can be exported to postcss.config.js
+               return [require('precss'), require('autoprefixer')];
+             }
+           }
+          }
+        ]
+      },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['babel-loader']
+        use: ['react-hot-loader', 'babel-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ]
 };
